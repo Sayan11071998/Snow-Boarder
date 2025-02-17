697a67b4425aacc546a4b3989577d4aeb0377c7a
Sayan Nandi <sayannandi11071998@gmail.com>
2025-02-18 01:50:03 +0530

    Particle System Added

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -4,11 +4,13 @@ using UnityEngine.SceneManagement;
 public class CrashDetector : MonoBehaviour
 {
     [SerializeField] private float loadDealy = 0.5f;
+    [SerializeField] private ParticleSystem crashEffect;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Ground")
         {
+            crashEffect.Play();
             Invoke("ReloadScene", loadDealy);
         }
     }
--- Assets/Scripts/FinishLine.cs
+++ Assets/Scripts/FinishLine.cs
@@ -4,11 +4,13 @@ using UnityEngine.SceneManagement;
 public class FinishLine : MonoBehaviour
 {
     [SerializeField] private float loadDealy = 1f;
+    [SerializeField] private ParticleSystem finishEffect;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Player")
         {
+            finishEffect.Play();
             Invoke("ReloadScene", loadDealy);
         }
     }
