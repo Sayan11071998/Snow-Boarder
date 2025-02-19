fec5c2e116ab11f02f7d4ce7904e8ea5030bdfea
Sayan Nandi <sayannandi11071998@gmail.com>
2025-02-19 13:14:51 +0530

    Skate Board Particle Effect Added

---
+++ Assets/Scripts/DustTrail.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class DustTrail : MonoBehaviour
+{
+    [SerializeField] private ParticleSystem dustParticles;
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Ground")
+        {
+            dustParticles.Play();
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Ground")
+        {
+            dustParticles.Stop();
+        }
+    }
+}
